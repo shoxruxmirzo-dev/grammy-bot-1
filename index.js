a919dc3ba30d8432f314c21454372b601fbf0821
Shoxruxmirzo Najmiddinov <165355089+shoxruxmirzo-dev@users.noreply.github.com>
2025-09-18 16:25:24 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const { Bot, GrammyError, HttpError, Keyboard, InlineKeyboard } = require('grammy');
+require('dotenv').config();
+
+const bot = new Bot(process.env.BOT_API_TOKEN);
+
+bot.command('start', async (ctx) => {
+  const startKB = new Keyboard().text('HTML').text('CSS').row().text('JS').text('React').resized();
+  await ctx.reply(
+    'Привет! Я - Фронтенд интервью бот 🤖 \nЯ помогу тебе подготовиться к интервью по фронтенду'
+  );
+  await ctx.reply('С чего начнем? Выбери тему вопроса в меню 👇', {
+    reply_markup: startKB,
+  });
+});
+
+bot.hears(['HTML', 'CSS', 'JS', 'React'], async (ctx) => {
+  const inlineKB = new InlineKeyboard()
+    .text(
+      'Получить ответ',
+      JSON.stringify({
+        question_id: 1,
+        type: ctx.message.text,
+      })
+    )
+    .text('Отменить', 'cancel');
+  await ctx.reply(`Что такое ${ctx.message.text}?`, {
+    reply_markup: inlineKB,
+  });
+});
+
+bot.on('callback_query:data', async (ctx) => {
+  if (ctx.callbackQuery.data === 'cancel') {
+    await ctx.reply('Отменено');
+    await ctx.answerCallbackQuery();
+    return;
+  }
+
+  const CBData = JSON.parse(ctx.callbackQuery.data);
+  await ctx.reply(`${CBData.type} - составляющая фронтенда`);
+  await ctx.answerCallbackQuery();
+});
+
+bot.catch((err) => {
+  const ctx = err.ctx;
+  console.error(`Ошибка при обработке обновления ${ctx.update.update_id}:`);
+  const e = err.error;
+  if (e instanceof GrammyError) {
+    console.error('Ошибка в запросе:', e.description);
+  } else if (e instanceof HttpError) {
+    console.error('Не удалось связаться с Telegram:', e);
+  } else {
+    console.error('Неизвестная ошибка:', e);
+  }
+});
+
+bot.start();
